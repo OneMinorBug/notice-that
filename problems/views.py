222b85b6d720fd0b8b1f7461fce2be69eab1b365
OneMinorBug <zhibosheng12@gmail.com>
2024-11-03 15:18:10 -0700

    Link solution and pinned comment, add unpin, fix bugs

--- problems/views.py
+++ problems/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.views.decorators.http import require_GET
@@ -8,11 +9,16 @@ from django.core.files.storage import default_storage
 from django.http import Http404, HttpResponse, JsonResponse
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils import timezone
 from .models import Problem, Comment
 from .forms import ProblemForm, CommentForm
 
 # Create your views here.
+User = get_user_model()
+updating_comment = False
+updating_problem = False
 
 def home(request):
     if not request.user.is_staff:
@@ -40,8 +46,8 @@ def problem_detail(request, pk):
         messages.error(request, "This problem is not available.")
         previous_page = request.META.get('HTTP_REFERER', '/')
         return redirect(previous_page)
-    show_solution = problem.solution_post_at and problem.solution_post_at <= timezone.now()
-    pinned_comment = problem.comments.filter(pinned=True, created_at__lte=timezone.now()).first()
+    show_solution = request.user.is_superuser or problem.solution_post_at <= timezone.now()
+    pinned_comment = problem.comments.filter(pinned=True).first()
     comments = problem.comments.filter(parent=None).exclude(id=pinned_comment.id if pinned_comment else None)   # Exclude pinned comment only if it exists
     user_has_commented = problem.comments.filter(account=request.user).exists() if request.user.is_authenticated else False
     reply_form = CommentForm()
@@ -60,13 +66,19 @@ def problem_detail(request, pk):
                 reply.parent = get_object_or_404(Comment, id=request.POST['comment_id'])  # Get parent comment
                 reply.save()
                 return redirect('problems:problem_detail', pk=problem.id)
-        elif 'pin_comment' in request.POST and request.user.is_staff:  # Pinning functionality for staff users
+            
+        elif 'pin_comment' in request.POST and request.user.is_staff:  # Pinning for staffs
             comment_to_pin = get_object_or_404(Comment, id=request.POST['pin_comment'])
             # Unpin any previously pinned comment
             Comment.objects.filter(problem=problem, pinned=True).update(pinned=False)
             comment_to_pin.pinned = True
             comment_to_pin.save()
             return redirect('problems:problem_detail', pk=problem.id)
+        
+        elif 'unpin_comment' in request.POST and request.user.is_staff:  # Unpinning for staffs
+            Comment.objects.filter(problem=problem, pinned=True).update(pinned=False)
+            return redirect('problems:problem_detail', pk=problem.id)
+        
         else:
             comment_form = CommentForm(request.POST)
             if comment_form.is_valid():
@@ -92,6 +104,47 @@ def problem_detail(request, pk):
         'show_solution': show_solution,
         'now': timezone.now(),
     })
+
+@receiver(post_save, sender=Problem)
+def update_solution_comment(sender, instance, created, **kwargs):
+    global updating_comment
+    if updating_comment:
+        return  # Exit if we're already updating to prevent recursion
+    
+    if not created and instance.solution_post_at:
+        updating_comment = True
+        try:
+            current_pinned_comment = instance.comments.filter(pinned=True).first()
+            if current_pinned_comment:
+                current_pinned_comment.content = instance.solution
+                current_pinned_comment.save()
+            else:
+                superuser = User.objects.filter(is_superuser=True).first()
+                if superuser and instance.solution != "<p><br></p>":
+                    Comment.objects.create(
+                        problem=instance,
+                        account=superuser,  # Use the account associated with the problem
+                        content=instance.solution,
+                        pinned=True,
+                        created_at=instance.solution_post_at
+                    )
+        finally:
+            updating_comment = False
+
+@receiver(post_save, sender=Comment)
+def update_problem_solution(sender, instance, created, **kwargs):
+    global updating_problem
+    if updating_problem:
+        return  # Exit if we're already updating to prevent recursion
+    try:
+        if instance.pinned:
+            # Update the related problem's solution if the pinned comment is updated
+            related_problem = instance.problem
+            related_problem.solution = instance.content
+            related_problem.solution_post_at = timezone.now()  # Update the time
+            related_problem.save()
+    finally:
+            updating_problem = False
 
 @login_required
 @staff_member_required
