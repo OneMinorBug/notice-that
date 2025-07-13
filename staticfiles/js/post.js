236e04e87137e0d61fe4f196b6ba091b0b2cc477
OneMinorBug <zhibosheng12@gmail.com>
2025-07-12 19:55:21 -0700

    I forgot to collect static

--- staticfiles/js/post.js
+++ staticfiles/js/post.js
@@ -2,86 +2,78 @@ window.wangEditor.i18nChangeLanguage('en')
 
 const { createEditor, createToolbar } = window.wangEditor
 
-const editorConfig = {
-    placeholder: 'Type your response here...',
-    onChange(editor) {
-        const html = editor.getHtml()
-        // Update the hidden textarea with the editor's content
-        document.getElementById('editor-content').value = html;
-    },
-    
-    // Image upload configuration
-    MENU_CONF: {
-        uploadImage: {
-            server: '/upload/',  // The URL for image uploads in Django
-            fieldName: 'file',    // The field name used in the Django view
-            maxFileSize: 1 * 1024 * 1024,  // Max file size (1MB)
-            allowedFileTypes: ['image/jpeg','image/png', 'image/heic'],  // Allowed file types
-            
-            // Callbacks
-            onSuccess(file, res) {
-                console.log('Image upload successful:', res)
-            },
-            onFailed(file, res) {
-                console.log('Image upload failed:', res)
-            },
-            onError(file, err, res) {
-                console.log('Image upload error:', err, res)
-            },
-        }
-    }
-}
+if (document.getElementById('editor-container')) {
 
-const editor = createEditor({
-    selector: '#editor-container',
-    html: '<p><br></p>',
-    config: editorConfig,
-    mode: 'simple',
-})
-
-const toolbarConfig = {}
-
-const toolbar = createToolbar({
-    editor,
-    selector: '#toolbar-container',
-    config: toolbarConfig,
-    mode: 'simple',
-})
-
-const solutionEditorConfig = {
-    placeholder: 'Type the solution here...',
-    onChange(editor) {
-        const html = editor.getHtml()
-        document.getElementById('solution-content').value = html;
-    },
-    MENU_CONF: {
-        uploadImage: {
-            server: '/upload/',  
-            fieldName: 'file',
-            maxFileSize: 1 * 1024 * 1024,
-            allowedFileTypes: ['image/jpeg', 'image/png', 'image/heic'],
-            onSuccess(file, res) {
-                console.log('Solution image upload successful:', res)
-            },
-            onFailed(file, res) {
-                console.log('Solution image upload failed:', res)
-            },
-            onError(file, err, res) {
-                console.log('Solution image upload error:', err, res)
+    const editorConfig = {
+        placeholder: 'Type your response here...',
+        onChange(editor) {
+            const html = editor.getHtml()
+            console.log('editor content', html)
+            // Update the hidden textarea with the editor's content
+            document.getElementById('editor-content').value = html;
+        },
+        
+        // Image upload configuration
+        MENU_CONF: {
+            uploadImage: {
+                server: '/upload/',  // The URL for image uploads in Django
+                fieldName: 'file',    // The field name used in the Django view
+                maxFileSize: 1 * 1024 * 1024,  // Max file size (1MB)
+                allowedFileTypes: ['image/jpeg','image/png', 'image/heic'],  // Allowed file types
+                // Callbacks
+                onSuccess(file, res) {console.log('Image upload successful:', res)},
+                onFailed(file, res) {console.log('Image upload failed:', res)},
+                onError(file, err, res) {console.log('Image upload error:', err, res)},
             }
         }
     }
+
+    // If a user submits an invalid form, Django will have already filled this textarea with their old content. If it's a new form, this will be empty.
+    const initialContent = document.getElementById('editor-content').value;
+
+    const editor = createEditor({
+        selector: '#editor-container',
+        html: initialContent,
+        config: editorConfig,
+        mode: 'simple',
+    })
+    
+    const toolbar = createToolbar({
+        editor,
+        selector: '#toolbar-container',
+        mode: 'simple',
+    })
 }
 
-const solutionEditorContainer = document.querySelector('#solution-editor-container');
+if (document.getElementById('solution-editor-container')) {
 
-if (solutionEditorContainer) {
+    const solutionEditorConfig = {
+        placeholder: 'Type the solution here...',
+        onChange(editor) {
+            const html = editor.getHtml()
+            document.getElementById('solution-content').value = html;
+        },
+        MENU_CONF: {
+            uploadImage: {
+                server: '/upload/',  
+                fieldName: 'file',
+                maxFileSize: 1 * 1024 * 1024,
+                allowedFileTypes: ['image/jpeg', 'image/png', 'image/heic'],
+                onSuccess(file, res) {console.log('Solution image upload successful:', res)},
+                onFailed(file, res) {console.log('Solution image upload failed:', res)},
+                onError(file, err, res) {console.log('Solution image upload error:', err, res)}
+            }
+        }
+    }
+
+    const initialSolutionContent = document.getElementById('solution-content').value;
+
     const solutionEditor = createEditor({
         selector: '#solution-editor-container',
-        html: '<p><br></p>',
+        html: initialSolutionContent,
         config: solutionEditorConfig,
         mode: 'simple',
-    });
+    })
 
     createToolbar({
         editor: solutionEditor,
