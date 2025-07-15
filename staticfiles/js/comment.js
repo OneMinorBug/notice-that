03417bca4e6e492b845be9a59e8257d9cee3b0d0
OneMinorBug <zhibosheng12@gmail.com>
2025-07-15 16:43:51 -0700

    Fomantic Form validation, improve code, Fomantic CDN

--- staticfiles/js/comment.js
+++ staticfiles/js/comment.js
@@ -1,14 +1,34 @@
-document.querySelectorAll('.reply').forEach(function (button) {
-    button.addEventListener('click', function () {
-        const commentId = this.getAttribute('data-comment-id');
-        const replyForm = document.getElementById('reply-form-' + commentId);
-        // Toggle visibility of the corresponding reply form
-        if (replyForm) {
-            if (replyForm.style.display === 'none' || replyForm.style.display === '') {
-                replyForm.style.display = 'block';
-            } else {
-                replyForm.style.display = 'none';
+$(document).ready(function() {
+    document.querySelectorAll('a.reply[data-comment-id]').forEach(function (button) {
+        button.addEventListener('click', function (event) {
+            // Prevent the default action of the button
+            event.preventDefault();
+            const commentId = this.getAttribute('data-comment-id');
+            const replyForm = document.getElementById('reply-form-' + commentId);
+
+            // Toggle visibility of the corresponding reply form
+            const isVisible = replyForm.style.display === 'block';
+            replyForm.style.display = isVisible ? 'none' : 'block'; 
+
+            // Check if this form has already been set up. We only do this once.
+            const isInitialized = replyForm.getAttribute('data-initialized');
+
+            // If the form is being opened for the first time
+            if (!isVisible && !isInitialized) {
+                $(`#reply-form-${commentId}`).form({
+                    on: 'blur', // Validate when the user clicks away from the textarea
+                    inline: true,
+                    fields: {
+                        content: {
+                            identifier: 'content',
+                            rules: [{type: 'notEmpty', prompt: 'Your reply cannot be empty.'}]
+                        }
+                    }
+                });
+
+                // Mark the form as initialized so we don't set it up again.
+                replyForm.setAttribute('data-initialized', 'true');
             }
-        }
+        });
     });
 });
