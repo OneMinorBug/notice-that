6af88938ceb4cf111e4df599a0c76ae509d71c1e
OneMinorBug <zhibosheng12@gmail.com>
2025-08-07 01:47:25 -0700

    Fix annoying bug in development

--- notice_that/urls.py
+++ notice_that/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
 from problems.views import view_log_file
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseNotFound
 
 #Sitemaps
 from django.contrib.sitemaps.views import sitemap
@@ -50,3 +50,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += re_path(r'^\.well-known/appspecific/com\.chrome\.devtools\.json$', lambda request: HttpResponseNotFound()),
